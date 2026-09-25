91caa12d96c5f9cc760efdae7c0dd4d63035c04c
TarekAXC <tariq.a.atrees@gmail.com>
2026-09-25 16:25:13 +0300

    "pleaseeeee speed I need thissss"

---
+++ main.js
@@ -0,0 +1,25 @@
+let person1 = {
+    name: "John",
+    age: 30,
+    skills: ["JavaScript", "React", "Node.js"],
+    phone: {
+        home: "123-456-7890",
+        work: "987-654-3210"
+    }
+}
+
+let person2 = {
+    name: "Jane",
+    age: 25,
+    skills: ["Python", "Django", "Flask"],
+    phone: {
+        home: "555-555-5555",
+        work: "444-444-4444"
+    }
+}
+
+console.log(person1)
+console.log(person2)
+console.log(person1.name)
+console.log(person2.skills[1])
+console.log(person1.phone.home)
